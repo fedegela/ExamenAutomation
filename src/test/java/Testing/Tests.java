3bb3ebca3a70cd6e37d28e95b7728b0bc1f5f8c7
fedegela <fede90g@hotmail.com>
2018-12-10 04:28:09 -0300

    Test?

--- src/test/java/Testing/Tests.java
+++ src/test/java/Testing/Tests.java
@@ -4,15 +4,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pageObject.MainPage;
 
-public class Testeo 
+public class Tests
 {
 	private WebDriver driver;
 	
 	@BeforeMethod
 	public void SetUp()
 	{
-        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver");
+        
+		String os = System.getProperty("os.name").toLowerCase();
+		if (os.contains("mac"))
+		{
+			System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver");
+		}
+		else 
+		{
+			System.setProperty("webdriver.chrome.driver","Drivers/chromedriver.exe");
+		}
 		driver = new ChromeDriver();	
 	}
 	
@@ -20,7 +30,7 @@ public class Testeo
 	public void BuscarZapatillas()
 	{
 		MainPage mainPage = new MainPage(driver);
-        driver.navigate().to(mainPage.getWebUrl);
+        driver.navigate().to(mainPage.getWebUrl());
 		mainPage.selectCategory();
 	}
 	
