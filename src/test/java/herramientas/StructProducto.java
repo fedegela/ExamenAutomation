d85b2b7da0ff8e8930dbb39cc5b8dcd8a585eba4
Federico <fede90g@hotmail.com>
2018-12-11 23:10:46 -0300

    Te odio ebay

---
+++ src/test/java/herramientas/StructProducto.java
@@ -0,0 +1,24 @@
+package herramientas;
+
+public class StructProducto 
+{
+	private String precio;
+	private String titulo;
+	
+	public String getPrecio() {
+		return precio;
+	}
+	
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+}
--- src/test/java/pageObject/MainPage.java
+++ src/test/java/pageObject/MainPage.java
@@ -1,10 +1,15 @@
 package pageObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
+
+import herramientas.StructProducto;
+
 
 public class MainPage 
 {
@@ -15,13 +20,17 @@ public class MainPage
 	private By buttonCalzado = By.partialLinkText("Calzado");
 	
 	//private By buttonCalzadoParaHombre = By.partialLinkText("Calzado para hombres");
-	private By dropdownOrden = By.id("w7-w0-w1_btn");
-	private By buttonPrecioMasBajo = By.xpath("*//span[contains(text(),'Precio más bajo')]");
+	private By dropdownOrden = By.cssSelector("[id$=w0-w1_btn]");
+	private By buttonPrecioMasBajo = By.xpath("*//span[contains(text(),'bajo')]");
 	
 	//private By buttonSearch = By.id("gh-btn"); No lo estoy usando por el momento
-	private By buttonEnvioGratis = By.partialLinkText("Envío internacional");
+	private By buttonEnvioGratis = By.partialLinkText("internacional");
 	private By buttonTalle10 = By.xpath("*//span[text()='10']");
 	private By buttonPuma = By.xpath("*//span[contains(text(),'PUMA')]");
+	
+	//private By claseCercaDeResultados = By.className("clearfix");
+	private By resultados = By.xpath("*//h2[contains(text(),'1 - ')]");
+	private int productosAMostrar = 5;
 	
 	public MainPage (WebDriver driver)
 	{
@@ -37,8 +46,26 @@ public class MainPage
 		driver.findElement(buttonEnvioGratis).click();
 		driver.findElement(buttonPuma).click();
 		Actions action = new Actions(driver);
+		System.out.println("Encontro " + cantidadDeResultados() + " resultados.");
 		action.moveToElement(driver.findElement(dropdownOrden)).build().perform();
 		driver.findElement(buttonPrecioMasBajo).click();
+		
+		List<WebElement> precios = driver.findElements(By.className("s-item__price"));
+		System.out.println(precios.size());
+		List<WebElement> titulos = driver.findElements(By.className("s-item__title"));
+		System.out.println(titulos.size());
+		List<StructProducto> productos = new ArrayList<StructProducto>();
+		for(int i=0; i < productosAMostrar; i++)
+		{
+			StructProducto productoNuevo = new StructProducto(); 
+			productoNuevo.setPrecio(precios.get(i).getText());
+			productoNuevo.setTitulo(titulos.get(i).getText());
+			productos.add(productoNuevo);
+			System.out.println("Producto numero: " + (i+1));
+			System.out.println("Titulo: " +  productos.get(i).getTitulo());
+			System.out.println("Precio " + ": " +  productos.get(i).getPrecio());
+		}
+		
 	}
 
     public String getWebUrl()
@@ -49,12 +76,28 @@ public class MainPage
     private WebElement getCalzadoParaHombre()
     {
     	By botonEspanol = By.partialLinkText("Calzado");
-    	By buttonEnglish = By.partialLinkText("Mens");
+    	By buttonEnglish = By.partialLinkText("Men's");
     	if (driver.findElement(botonEspanol).isEnabled())
     		return driver.findElement(botonEspanol);
     	else 
     		return driver.findElement(buttonEnglish);
     }
-
-
+    
+    private String cantidadDeResultados()
+    {
+		String resultado = driver.findElement(resultados).getText();
+		resultado = resultado.substring(0, resultado.lastIndexOf(" "));
+		return resultado.substring(resultado.lastIndexOf(" ")+1);
+    }
 }
+
+
+
+/*
+ * si el resultado aparece en negrita, el numero de resultados esta en un elemento diferente a la palabra resultados.
+ * si el resultado NO esta en negrita, la cantidad y la palabra resultados estan en el mismo elemento.
+ * 
+ * 
+ * 
+ * 
+ */
